ac9afd2b6170a9c0cba02920da878b8051828167
Patricio Armesto <patricio.armesto@mercadolibre.com>
2021-03-29 14:09:16 -0300

    Add comments

--- main.go
+++ main.go
@@ -14,14 +14,17 @@ func main() {
 
 	buf := new(bytes.Buffer)
 
+	// Selects folder (Scenario) to run (info comming from queue)
 	opts := godog.Options{
 		Output: colors.Colored(buf),
 		Format: "pretty",
 		Paths:  []string{"taxes"},
 	}
 
+	// Run tests
 	status := helpers.RunTests(opts)
 
+	// Save this output to database
 	fmt.Println(buf.String())
 
 	os.Exit(status)
